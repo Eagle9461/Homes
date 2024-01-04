633381b8e296bec4012de22672386c98a5017083
DESKTOP-9NC8JV4\K <eagle9461@outlook.com>
2024-01-04 18:32:38 -0300

    service for the data pulling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,10 @@ import { HousingLocationComponent } from './housing-location/housing-location.co
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HomeComponent, HousingLocationComponent],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-  
-  `,
+  templateUrl: './app.component.html',
   // templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'Homes';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { HousingLocation } from '../housinglocation';
+
+import { HousingService } from '../housing.service';
+
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [ CommonModule, HousingLocationComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,14 +18,10 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
